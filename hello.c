2a10497a36ffe75da25676bb5ecfa5ad3e6cb682
Gxt200002 <72630570+Gxt200002@users.noreply.github.com>
2021-03-11 14:37:54 -0600

    Update hello.c

--- hello.c
+++ hello.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>   
 int main()
 {
+	printf("Entering hello.c");
 	printf("Hello World\n");   // hello Richard
+	printf("Leaving hello.c");
  	exit(0);         // end of the program
 }
